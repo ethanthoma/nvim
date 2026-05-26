2cb7881b127df3dc4c86855a1194f64f3eee962b
Ethan Thoma <ethanthoma@gmail.com>
2026-05-26 12:02:50 -0700

    feat: use verusfmt for rust files with verus! macro

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,7 +5,12 @@ return {
     opts = {
         formatters_by_ft = {
             nix = { "nixfmt" },
-            rust = { "rustfmt" },
+            rust = function(bufnr)
+                for _, line in ipairs(vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)) do
+                    if line:match("verus!") then return { "verusfmt" } end
+                end
+                return { "rustfmt" }
+            end,
             gleam = { "gleam" },
             json = { "jq" },
             javascript = { "biome" },
@@ -29,6 +34,11 @@ return {
         },
 
         formatters = {
+            verusfmt = {
+                command = "verusfmt",
+                args = { "$FILENAME" },
+                stdin = false,
+            },
             typstyle = {
                 command = "typstyle",
                 stdin = true,
