7d80184c3f6be21249519907113020cd1d91610b
Ethan Thoma <ethanthoma@gmail.com>
2026-06-08 22:56:39 -0700

    deps: update ts

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -1,6 +1,6 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    branch = "main",
+    "neovim-treesitter/nvim-treesitter",
+    dependencies = { "neovim-treesitter/treesitter-parser-registry" },
     build = ":TSUpdate",
     lazy = false,
     config = function()
